2ff5557a6d0d42193333c2ae4fad14127283a66c
Your Name <you@example.com>
2019-02-12 15:36:33 +0700

    commit

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Product extends React.Component{
+    render(){
+        return (
+                <div>
+                <h1>{this.props.judul}</h1>
+                <h3>{this.props.harga}</h3>
+                </div>);
+    }
+}
+
+export default Product;
